dd8a03eb6a1f21b5f7e537b86baf3e75f872b506
taszhanov <77776408+taszhanov@users.noreply.github.com>
2021-03-13 17:11:45 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+var http = require('http');
+var fs = require("fs");
+
+function serveStaticFile(res, path, contentType, responseCode) {
+    if (!responseCode) responseCode = 200;
+    fs.readFile(__dirname + path, function(err, data) {
+        if (err) {
+            res.writeHead(500, {"Content-Type": "text/plain"})
+            res.end("500 - Internal error")
+        }
+        else {
+            res.writeHead(responseCode, {"Content-Type": contentType})
+            res.end(data)
+        }
+    })
+}
+
+http.createServer(function(req, res){
+    var path = req.url.replace(/\/?(?:\?.*)?$/ ,"").toLowerCase()
+    switch(path) {
+        case "":
+            serveStaticFile(res, "/index.html", "text/html")
+            break;
+        case "/about":
+            serveStaticFile(res, "/about.html", "text/html")
+            break;
+        case "/video/memes":
+            serveStaticFile(res, "/video/students/memes.mp4", "video/mp4")
+            break;
+        case "/img/gallery/graduation":
+            serveStaticFile(res, "/img/gallery/graduation.jpg", "image/jpeg")
+            break;
+        case "/img/gallery/study":
+            serveStaticFile(res, "/img/gallery/study.jpg", "image/jpeg")
+            break;
+        default:
+            serveStaticFile(res, "/error.html", "text/html", 404)
+            break;
+    }
+}).listen(3000);
+
+console.log('Server started on localhost:3000; press Ctrl-C to terminate....');
